056fa156686901fba3186049f7912eae09b7a895
Efe Emre İpek <ipekefeemre@gmail.com>
2025-07-04 11:50:43 +0300

    added moving vehicles on road

---
+++ Assets/Scripts/VehicleMover.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class VehicleMover : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float bound;
+    [SerializeField] private int direction; // 1: right 0: left
+
+    private void Update()
+    {
+        transform.Translate(speed * Time.deltaTime * (direction == 0 ? -transform.right : transform.right), Space.Self);
+
+        if(transform.position.x < -bound)
+        {
+            transform.position = new Vector3(bound, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > bound)
+        {
+            transform.position = new Vector3(-bound, transform.position.y, transform.position.z);
+        }
+    }
+}
