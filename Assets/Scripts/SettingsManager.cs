9b4b5bc1c5db36e8105862a95c0f78b12eff723d
Efe Emre İpek <ipekefeemre@gmail.com>
2025-07-08 15:04:28 +0300

    added resolution and fulscreen settings

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -11,7 +13,32 @@ public class SettingsManager : MonoBehaviour
     [SerializeField] private Slider SFXSlider;
     [SerializeField] private TMP_Text musicText;
     [SerializeField] private TMP_Text SFXText;
+    private float musicVolume;
+    private float SFXVolume;
 
+    [Header("Resolution")]
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    private List<string> resolutionOptions = new List<string>
+    {
+        "1024 x 576",
+        "1280 x 720",
+        "1366 x 768",
+        "1600 x 900",
+        "1920 x 1080",
+        "2560 x 1440",
+        "3840 x 2160",
+    };
+    private int resolutionWidth, resolutionHeight;
+
+    [Header("Fullscreen")]
+    [SerializeField] private TMP_Dropdown fullScreenDropdown;
+    private List<string> fullScreenOptions = new List<string>
+    {
+        "Exclusive Fullscreen",
+        "Borderless Window",
+        "Windowed"
+    };
+    private FullScreenMode fullScreenMode;
 
     private void Start()
     {
@@ -20,32 +47,109 @@ public class SettingsManager : MonoBehaviour
 
         musicText.text = Mathf.RoundToInt(musicSlider.value * 100).ToString();
         SFXText.text = Mathf.RoundToInt(SFXSlider.value * 100).ToString();
+
+        SetResolutionCurrent();
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+
+        SetFullScreenCurrent();
+        fullScreenDropdown.onValueChanged.AddListener(SetFullScreen);
+    }
+
+    public void ApplyButton()
+    {
+        if(musicVolume <= 0.001f) audioMixer.SetFloat("MusicVolume", -80f);
+        else audioMixer.SetFloat("MusicVolume", Mathf.Log10(musicVolume) * 20);
+
+        if(SFXVolume <= 0.001f) audioMixer.SetFloat("SFXVolume", -80f);
+        else audioMixer.SetFloat("SFXVolume", Mathf.Log10(SFXVolume) * 20);
+
+        Screen.SetResolution(resolutionWidth, resolutionHeight, fullScreenMode);
     }
 
     public void SetMusicVolume(float volume)
     {
-        if(volume <= 0.001f)
-        {
-            audioMixer.SetFloat("MusicVolume", -80f);
-        }
-        else
-        { 
-            audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
-        }
-
+        musicVolume = volume;
         musicText.text = Mathf.RoundToInt(musicSlider.value * 100).ToString();
     }
     public void SetSFXVolume(float volume)
     {
-        if(volume <= 0.001f)
+        SFXVolume = volume;
+        SFXText.text = Mathf.RoundToInt(SFXSlider.value * 100).ToString();
+    }
+    public void SetResolution(int index)
+    {
+        string[] parts = resolutionDropdown.options[index].text.Split('x');
+        resolutionWidth = int.Parse(parts[0].Trim());
+        resolutionHeight = int.Parse(parts[1].Trim());
+    }
+    private void SetResolutionCurrent()
+    {
+        resolutionDropdown.ClearOptions();
+        resolutionDropdown.AddOptions(resolutionOptions);
+
+        int currentWidth = Screen.width;
+        int currentHeight = Screen.height;
+        bool foundResolution = false;
+
+        for(int i = 0; i < resolutionOptions.Count; i++)
         {
-            audioMixer.SetFloat("SFXVolume", -80f);
-        }
-        else
-        {
-            audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+            string[] parts = resolutionOptions[i].Split('x');
+            int w = int.Parse(parts[0].Trim());
+            int h = int.Parse(parts[1].Trim());
+
+            if(w == currentWidth && h == currentHeight)
+            {
+                resolutionDropdown.value = i;
+
+                resolutionWidth = w;
+                resolutionHeight = h;
+
+                foundResolution = true;
+                break;
+            }
         }
 
-        SFXText.text = Mathf.RoundToInt(SFXSlider.value * 100).ToString();
+        if(!foundResolution)
+        {
+            resolutionOptions.Add($"{currentWidth} x {currentHeight}");
+            resolutionDropdown.ClearOptions();
+            resolutionDropdown.AddOptions(resolutionOptions);
+            resolutionDropdown.value = resolutionOptions.Count - 1;
+
+            resolutionWidth = currentWidth;
+            resolutionHeight = currentHeight;
+        }
+    }
+    public void SetFullScreen(int index)
+    {
+        switch(index)
+        {
+            case 0:
+                fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+            case 1:
+                fullScreenMode = FullScreenMode.FullScreenWindow;
+                break;
+            case 2:
+                fullScreenMode = FullScreenMode.Windowed;
+                break;
+        }
+    }
+    private void SetFullScreenCurrent()
+    {
+        fullScreenDropdown.ClearOptions();
+        fullScreenDropdown.AddOptions(fullScreenOptions);
+
+        FullScreenMode current = Screen.fullScreenMode;
+
+        int index = 1;
+        switch(current)
+        {
+            case FullScreenMode.ExclusiveFullScreen: index = 0; fullScreenMode = FullScreenMode.ExclusiveFullScreen; break;
+            case FullScreenMode.FullScreenWindow:    index = 1; fullScreenMode = FullScreenMode.FullScreenWindow; break;
+            case FullScreenMode.Windowed:            index = 2; fullScreenMode = FullScreenMode.Windowed; break;
+        }
+
+        fullScreenDropdown.value = index;
     }
 }
